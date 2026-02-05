5fb002d552c3859e30b78ef2474010e451450be0
wahb-amir <op422010@gmail.com>
2026-02-05 22:24:29 +0500

    update

--- app/Component/Avatar.tsx
+++ app/Component/Avatar.tsx
@@ -4,15 +4,21 @@ import Image from "next/image";
 
 export default function Avatar() {
   return (
-    <figure aria-hidden="true" className="mx-auto">
-      <Image
-        src="/Avatar.svg"
-        alt=""
-        width={300}
-        height={300}
-        priority
-        className="rounded-full border-4 border-cyan-400 shadow-lg"
+    <div className="relative group">
+      <div
+        className="absolute -inset-1 bg-gradient-to-r from-blue-600 to-cyan-400 rounded-full blur opacity-25 group-hover:opacity-50 transition duration-1000 group-hover:duration-200"
+        aria-hidden="true"
       />
-    </figure>
+      <figure className="relative mx-auto rounded-full p-1 bg-white dark:bg-slate-900 ring-1 ring-slate-900/5 dark:ring-white/10 overflow-hidden">
+        <Image
+          src="/Avatar.svg"
+          alt="Wahb"
+          width={300}
+          height={300}
+          priority
+          className="rounded-full object-cover w-full h-full"
+        />
+      </figure>
+    </div>
   );
 }
